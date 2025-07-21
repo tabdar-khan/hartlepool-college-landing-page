966482774641d68b90f6e966ceead2778bd8f0ec
tabdar.khan@gmail.com <tabdar.khan@gmail.com>
2025-07-21 21:46:30 +0200

    Change Navigation + Adjust scroll bouncer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero = () => {
         </div>
 
         {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="absolute left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
             <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
           </div>
